3dad2432f0103ecf7c3ed2363b2a6259b77e9923
Jurriaan Voskes <jurriaan.voskes@gmail.com>
2014-05-01 13:48:46 +0200

    added laser angle cap difference based on control::NAV_STATE for during forward motion and obstacle avoidance

--- skynav_control/src/data_verifier.cpp
+++ skynav_control/src/data_verifier.cpp
@@ -5,6 +5,8 @@
 #include <sensor_msgs/Range.h>
 #include <sensor_msgs/LaserScan.h>
 #include <skynav_msgs/current_pose.h>
+#include <std_msgs/UInt8.h>
+
 
 #define SIMULATOR 						true 	// set this to true when using the simulator, false indicates the actual robot with lower specs is used
 
@@ -19,6 +21,7 @@ ros::Publisher pubLaserFiltered;
 
 ros::ServiceClient servClientCurrentPose;
 
+uint8_t mControl_NavigationState = 0;				//the state which motion_control is in.
 
 Pose getCurrentPose() {
 
@@ -34,6 +37,11 @@ Pose getCurrentPose() {
     return currentPose;
 }
 
+void subNavigationStateCallback(const std_msgs::UInt8& msg ){
+	mControl_NavigationState = msg.data;
+	ROS_INFO("localnav NavigationState: %d", msg.data);	
+}
+
 void subSensorsCallback(const skynav_msgs::RangeDefinedArray::ConstPtr& msg) {
     
     // check limits here
@@ -44,7 +52,7 @@ void subSensorsCallback(const skynav_msgs::RangeDefinedArray::ConstPtr& msg) {
 void subRelativePoseCallback(const geometry_msgs::Pose::ConstPtr& msg)        {
     
     // check limits here
-    
+    ROS_INFO("odom");
     pubOdometry.publish(msg);    
 }
 
@@ -56,7 +64,8 @@ void subLaserScanCallback(const LaserScan::ConstPtr& msg)	{
 
 //filter the (raw)LaserScan data coming from the neato
 void subFilterLaserCallback(const LaserScan::ConstPtr& scan) {
-
+	int lLimit;
+	int hLimit;
 	LaserScan scan_filtered;
 	scan_filtered.header.frame_id = scan->header.frame_id;
 	scan_filtered.header.stamp = scan->header.stamp;
@@ -73,10 +82,28 @@ void subFilterLaserCallback(const LaserScan::ConstPtr& scan) {
 		if(!SIMULATOR){
 			//limit the laser angle on the actual robot so the robot itself wont be seen as object.
 			//the lds has been mounted with a 90 degree offset relative to the robot.. which results in the 0 degree (front) of the robot being at the 270 degree of the lds output
-
-			//if(i>30 && i<150){	//cap the laser so the robot wont see itself
-			//if(i<180){			//cap the laser so the robot only sees 180Degree in front of itself
-			if(i<225 || i>315){ 	//cap the laser to a 90 degree wide beam in front of the robot
+			
+			if(mControl_NavigationState == 2 || mControl_NavigationState == 6){
+				lLimit = 180; //cap the laser so the robot only sees 180Degree in front of itself
+				hLimit = 360;
+			}else{	
+				lLimit = 225; //cap the laser to a 90 degree wide beam in front of the robot
+				hLimit = 315;
+			}
+			if(i<lLimit || i>hLimit){				//if(i>30 && i<150){	//cap the laser so the robot wont see itself
+				scan_filtered.ranges.push_back( 0 );	// add 0 if outside limit or it ruins the rest of the angles (relient on array size)
+				scan_filtered.intensities.push_back( 0 );
+				continue;
+			}
+		}else{
+			if(mControl_NavigationState == 2 || mControl_NavigationState == 6){
+				lLimit = 90; //cap the laser so the robot only sees 180Degree in front of itself
+				hLimit = 270;
+			}else{	
+				lLimit = 135; //cap the laser to a 90 degree wide beam in front of the robot
+				hLimit = 215;
+			}
+			if(i<lLimit || i>hLimit){				//if(i>30 && i<150){	//cap the laser so the robot wont see itself
 				scan_filtered.ranges.push_back( 0 );	// add 0 if outside limit or it ruins the rest of the angles (relient on array size)
 				scan_filtered.intensities.push_back( 0 );
 				continue;
@@ -105,21 +132,20 @@ int main(int argc, char **argv) {
     ros::NodeHandle n("/control");
     ros::NodeHandle n_robot("/x80sv");
     ros::NodeHandle n_SLAM("/slam");
-
-
+    
     //pubs
     pubOdometry = n.advertise<geometry_msgs::Pose>("odometry", 32);
     pubSensors = n.advertise<skynav_msgs::RangeDefinedArray>("sensors", 1024);
-    pubLaser = n.advertise<sensor_msgs::LaserScan>("laser_scan", 1024);
-    
+    pubLaser = n.advertise<sensor_msgs::LaserScan>("laser_scan", 1024);    
 	pubLaserFiltered = n_robot.advertise<LaserScan>("laser/scan_filtered", 1000);
+	
     //subs
-    ros::Subscriber subSensors = n_robot.subscribe("sensors", 1024, subSensorsCallback);
-    
+    ros::Subscriber subSensors = n_robot.subscribe("sensors", 1024, subSensorsCallback);    
     ros::Subscriber subRelativePose = n_robot.subscribe("odometry", 32, subRelativePoseCallback);
     ros::Subscriber subLaserRobot = n_robot.subscribe("laser/scan_filtered", 1000, subLaserScanCallback);	
-	ros::Subscriber subLaser = n_robot.subscribe("laser/scan", 1000, subFilterLaserCallback);
-	
+	ros::Subscriber subLaser = n_robot.subscribe("laser/scan", 1000, subFilterLaserCallback);	
+	ros::Subscriber	subNavigationState= n.subscribe("navigation_state",0,subNavigationStateCallback);
+
 	//services
     servClientCurrentPose = n_SLAM.serviceClient<skynav_msgs::current_pose>("current_pose");
     
