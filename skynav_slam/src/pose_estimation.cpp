5274f734cfa1ddfd6515ab0cac93d2ea82269161
Windel Bouwman <windel.bouwman@gmail.com>
2014-06-11 10:38:47 +0200

    Refactor launch files with if statement

--- skynav_slam/src/pose_estimation.cpp
+++ skynav_slam/src/pose_estimation.cpp
@@ -8,12 +8,9 @@
 #include <std_msgs/Header.h>
 #include <geometry_msgs/PoseStamped.h>
 
-#define SIMULATOR 						true 	// set this to true when using the simulator, false indicates the actual robot with lower specs is used
 
 using namespace std;
 using namespace geometry_msgs;
-
-bool mFirstDataSkipped = false; // this is because ROS or the X80 (not known which) will often report a change of large degrees that are not correct. This will filter it out
 
 ros::ServiceServer servServerCurrentPose, servServerCurrentVelocity;
 ros::Publisher pubTraversedPath;
@@ -23,20 +20,9 @@ nav_msgs::Path mTraversedPath;
 
 tf::TransformListener* mTransformListener;
 
-void publishCurrentPoseTF() {
 
-    
-    // tf current pose
-    static tf::TransformBroadcaster br;
-    tf::Transform transform;
-    transform.setOrigin(tf::Vector3(mCurrentPose.position.x, mCurrentPose.position.y, mCurrentPose.position.z));      // derp ROS... must convert Point to Vector3 manually, even though they are *exactly* the same
-    transform.setRotation(tf::Quaternion(mCurrentPose.orientation.x, mCurrentPose.orientation.y, mCurrentPose.orientation.z)); // same as above... geometry_msgs::Quaternion and tf::Quaternion... derp
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/map", "/base_link"));
-}
-
-
-
-bool servServerCurrentPoseCallback(skynav_msgs::current_pose::Request &request, skynav_msgs::current_pose::Response &response) {
+bool servServerCurrentPoseCallback(skynav_msgs::current_pose::Request &request, skynav_msgs::current_pose::Response &response)
+{
 
     //response.pose = mCurrentPose;
     
@@ -44,9 +30,12 @@ bool servServerCurrentPoseCallback(skynav_msgs::current_pose::Request &request, 
 
     tf::StampedTransform st;
     
-    try {
+    try
+    {
 		mTransformListener->lookupTransform("/map", "/base_link", ros::Time(0), st);
-	}	catch (tf::TransformException ex)	{
+	}
+    catch (tf::TransformException ex)
+    {
 		ROS_ERROR("%s", ex.what());
 		return false;
 	}
@@ -66,9 +55,12 @@ bool servServerCurrentVelocityCallback(skynav_msgs::current_velocity::Request &r
 	
 	Twist tw;
 	
-	try{
+	try
+    {
 		mTransformListener->lookupTwist("/base_link", "/map", ros::Time(0), ros::Duration(1), tw);
-	}catch (tf::TransformException ex){
+	}
+    catch (tf::TransformException ex)
+    {
 		ROS_ERROR("%s", ex.what());
 		return false;
 	}
@@ -80,58 +72,20 @@ bool servServerCurrentVelocityCallback(skynav_msgs::current_velocity::Request &r
 	return true;
 }
 
-void subRelativePoseCallback(const geometry_msgs::Pose::ConstPtr& msg) {		// this is currently only called by the actual robot, not simulator
 
-    if (mFirstDataSkipped) {
-        if (msg->position.x != 0.0 || msg->position.y != 0.0 || msg->orientation.z != 0.0) {
-
-            double distance = msg->position.x;
-            double angle = msg->orientation.z;
-
-            double x = mCurrentPose.position.x;
-            double y = mCurrentPose.position.y;
-            double theta = mCurrentPose.orientation.z;
-
-            // recalculate pose
-            theta += angle;
-            x += cos(theta) * distance;
-            y += sin(theta) * distance;
-
-
-            // save new pose
-            mCurrentPose.position.x = floor(x * 1000) / 1000;    // truncate to mm
-            mCurrentPose.position.y = floor(y * 1000) / 1000;
-            mCurrentPose.orientation.z = theta; // dont truncate theta
-            
-            // add new pose to traversed path
-            PoseStamped ps;
-            ps.header.frame_id = "/map";
-            ps.header.stamp = ros::Time::now();
-            ps.pose = mCurrentPose;
-            
-            mTraversedPath.poses.push_back(ps);
-            
-            publishCurrentPoseTF();
-
-            //ROS_INFO("pose updated! x: %f, y: %f, theta: %.2f degrees", mCurrentPose.position.x, mCurrentPose.position.y, (mCurrentPose.orientation.z * 180 / M_PI));
-
-        }
-    } else {
-        mFirstDataSkipped = true;
-    }
-
-}
-
-
-void publishTraversedPath()     {
+void publishTraversedPath()
+{
 	
 	PoseStamped ps;
 	
 	tf::StampedTransform st;
     
-    try {
+    try
+    {
 		mTransformListener->lookupTransform("/map", "/base_link", ros::Time(0), st);
-	}	catch (tf::TransformException ex)	{
+	}
+    catch (tf::TransformException ex)
+    {
 		ROS_ERROR("%s", ex.what());
 		return;
 	}
@@ -150,7 +104,8 @@ void publishTraversedPath()     {
     pubTraversedPath.publish(mTraversedPath);    
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     ros::init(argc, argv, "pose_estimation");
 
@@ -162,12 +117,8 @@ int main(int argc, char **argv) {
     mTraversedPath.header.stamp = ros::Time::now();
     mTransformListener = new tf::TransformListener();
     
-    
     // pubs
     pubTraversedPath = n.advertise<nav_msgs::Path>("traversed_path", 32);
-
-    //subs
-    ros::Subscriber subRelativePose = n_control.subscribe("odometry", 8, subRelativePoseCallback);
 
     //services
     servServerCurrentPose = n.advertiseService("current_pose", servServerCurrentPoseCallback);
@@ -176,17 +127,14 @@ int main(int argc, char **argv) {
 
     ros::Rate loop_rate(10);    //10hz
     
-    while (ros::ok()) {
+    while (ros::ok())
+    {
 
         ros::spinOnce();
         
-        if(!SIMULATOR){
-			publishCurrentPoseTF(); //only use with real robot. in the simulator this function causes massive errors
-		}
         publishTraversedPath();
 
         loop_rate.sleep();
-
     }
     
     delete mTransformListener;
